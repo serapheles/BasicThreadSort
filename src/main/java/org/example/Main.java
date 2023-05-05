9ecee89da9fd66df4a77215dab17fcd238303607
serapheles <katathis@gmail.com>
2023-05-04 18:27:32 -0700

    Added threaded mergesort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        int size = 1 << 20;
+        Integer[] numbers = new Integer[size];
+        Random dice = new Random();
+        for(int i = 0;i < size;i++){
+            numbers[i] = dice.nextInt(1000);
+        }
+        long startTime = System.nanoTime();
+//        NewMerge.newSort(numbers);
+        ThreadMergeSort.mergeSort(numbers);
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+//        System.out.println(Arrays.toString(numbers));
+
     }
 }
